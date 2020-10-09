bb683efc8891cee4a363a5ed04e4efd583f1ec56
Simon H <simon.hostettler@protonmail.com>
2020-10-09 09:14:57 +0200

    Adjusted variable names to make their function clearer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter.filedialog import askopenfilename
 from PIL import Image
-import sys
 import numpy as np
+import sys
 
 NEW_IMG_HEIGHT = 100
 NEW_IMG_NAME = "DiceImage.jpg"
@@ -20,15 +20,15 @@ def convert_to_dice_image(filename):
 
     pixel_matrix = np.array(small_img)
     new_size = (int(NEW_IMG_HEIGHT * relative_size * 32) - 32, NEW_IMG_HEIGHT * 32)
-    dice_img = Image.new('L', new_size, color = 'white')
+    dice_img = Image.new('L', new_size, color='white')
 
     for row in range(0, NEW_IMG_HEIGHT):
         for column in range(0, int(NEW_IMG_HEIGHT * relative_size)):
-            greyval = pixel_matrix[row][column]
-            die_face = assign_dice_to_color(greyval)
+            grey_val = pixel_matrix[row][column]
+            die_face = assign_dice_to_color(grey_val)
             dice_img.paste(die_face, (column*32, row*32))
 
-    dice_img.save(NEW_IMG_NAME, quality = 100)
+    dice_img.save(NEW_IMG_NAME, quality=100)
     print("Done!")
     print("In real life this image would measure:"
           f" {int((1.6*NEW_IMG_HEIGHT*relative_size))}cm x {int((1.6*NEW_IMG_HEIGHT))}cm.")
@@ -39,5 +39,5 @@ def assign_dice_to_color(grey_value):
 
 
 if __name__ == "__main__":
-    filename = askopenfilename()
-    convert_to_dice_image(filename)
+    image_name = askopenfilename()
+    convert_to_dice_image(image_name)
